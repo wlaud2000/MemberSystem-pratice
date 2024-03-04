f71abaf8a682aee1d73d480bad2aae03548ec786
Jimyeong Kim <kjm021221@icloud.com>
2024-03-04 14:20:30 +0900

    feat: 회원 정보 상세 조회 기능
    REST API URL로 적용해봄. @PathVariable 사용
    id에 해당하는 회원의 정보를 DB에서 가져와서 그 값을 다시 화면으로 가져가는 로직

--- src/main/java/com/study/member/controller/MemberController.java
+++ src/main/java/com/study/member/controller/MemberController.java
@@ -6,10 +6,7 @@ import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -59,4 +56,12 @@ public class MemberController {
         model.addAttribute("memberList", memberDTOList);
         return "list";
     }
+
+    @GetMapping("/member/{id}")
+    public String findById(@PathVariable Long id, Model model) {
+        MemberDTO memberDTO = memberService.findById(id);
+        model.addAttribute("member", memberDTO);
+        return "detail";
+    }
+
 }
